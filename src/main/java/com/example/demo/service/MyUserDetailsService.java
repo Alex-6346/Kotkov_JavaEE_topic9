6127bb8f643a54f510316e73fa9ea1afe34954ad
Oleh Kurpiak <oleh.kurpiak@gmail.com>
2022-05-05 13:15:54 +0300

    fix security

--- src/main/java/com/example/demo/service/MyUserDetailsService.java
+++ src/main/java/com/example/demo/service/MyUserDetailsService.java
@@ -10,12 +10,10 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
 import java.util.stream.Collectors;
 import java.util.List;
 
-@Service
 @RequiredArgsConstructor
 public class MyUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
